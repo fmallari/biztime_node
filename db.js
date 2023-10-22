715719bf38bf8dc6561caf07a522244611a8a4c4
fmallari <fc.mallari@gmail.com>
2023-10-22 00:40:39 -0700

    add company routes

--- db.js
+++ db.js
@@ -1,23 +1,18 @@
 /** Database setup for BizTime. */
 const { Client } = require("pg");
 
-const client = new Client({
-    connectionString: "postgresql://biztime"
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+    DB_URI = "postgresql://biztime_test";
+} else {
+    DB_URI = "postgresql://biztime";
+}
+
+let db = new Client ({
+    connectionString: DB_URI 
 });
-// let DB_URI;
 
-// if (process.env.NODE_ENV === "test") {
-//     DB_URI = "postgresql://biztime_test";
-// } else {
-//     DB_URI = "postgresql://biztime";
-// }
-
-// let db = new Client ({
-//     connectionString: DB_URI 
-// });
-
-// db.connect();
-
-client.connect();
+db.connect();
 
 module.exports = client;
